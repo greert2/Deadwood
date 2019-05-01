d8a0e84ff2e085c35c05989259718ddfc575fbdf
greert2 <greert2@wwu.edu>
2019-05-01 13:46:46 -0700

    Added CastingOffice.java

---
+++ CastingOffice.java
@@ -0,0 +1,12 @@
+public class CastingOffice {
+  private int[][] upgrades;
+
+  public String[] getUpgrades(int money, int credits) {
+    String[] availUpgrades;
+    return availUpgrades;
+  }
+
+  public void selectUpgrade(String upgrade) {
+    return;
+  }
+}
