da09659172abc0215ff2b9c19a5f3ae45d19dd83
Brooks Perez <perezb3@wwu.edu>
2019-05-02 15:25:27 -0700

    Added Player.java

---
+++ Player.java
@@ -0,0 +1,61 @@
+
+public class Player {
+	private int rank;
+	private int dollars;
+	private int credits;
+	private Room currentRoom;
+	private int rehearseChips;
+	private Role role;
+	private String color;
+		
+	public void moveRoom(Room room) {
+		//TODO
+	}
+	
+	public void addMoney(int amount) {
+		//TODO
+	}
+	
+	public void addCredits(int amount) {
+		//TODO
+	}
+	
+	public void updateRole(Role role) {
+		//TODO
+	}
+	
+	public void updateRank(int rank) {
+		//TODO
+	}
+	
+	public void act() {
+		//TODO
+	}
+	
+	public void rehearse() {
+		//TODO
+	}
+	
+	public void givePlayerMainBonus(int roll, Role role) {
+		//TODO
+	}
+	
+	public void givePlayerExtraBonus(int roll, Role role) {
+		//TODO
+	}
+	
+	public String getColor() {
+		//TODO
+		return color;
+	}
+	
+	public int getMoney() {
+		//TODO
+		return dollars;
+	}
+	
+	public int getCredits() {
+		//TODO
+		return credits;
+	}
+}
