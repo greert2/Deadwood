7700f3800b134697052a6784b54bec9e24e82e35
Brooks Perez <perezb3@wwu.edu>
2019-05-02 15:27:55 -0700

    Added System.java

---
+++ System.java
@@ -0,0 +1,41 @@
+
+public class System {
+	private int playerCnt;
+	private Player[] players;
+	private int numCnt;
+	private int dayCnt;
+	private int dayTot;
+	private Player whosTurn;
+		
+	public boolean selectRoom(Room room) {
+		//TODO
+		return true;
+	}
+	
+	public void takeMoney(Player player, int amount) {
+		//TODO
+	}
+	
+	public int getDaysLeft() {
+		//TODO
+		return dayTot;
+	}
+
+	public int getDay() {
+		//TODO
+		return dayCnt;
+	}
+	
+	public Player nextTurn() {
+		//TODO
+		return whosTurn;
+	}
+	
+	public void calculateScores(Player[] players) {
+		//TODO
+	}
+	
+	
+	
+	
+}
