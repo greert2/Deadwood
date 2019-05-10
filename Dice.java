2d6d820b86380558a7ec009c559712789e04d9b7
Brooks Perez <perezb3@wwu.edu>
2019-05-09 17:55:43 -0700

    Added constructor and roll method

--- Dice.java
+++ Dice.java
@@ -1,17 +1,17 @@
 
 public class Dice {
-   final int MAX_DICE_VALUE = 6;
-   final int MIN_DICE_VALUE = 1;
-   private int numberOfDice;
+    final int MAX_DICE_VALUE = 6;
+    final int MIN_DICE_VALUE = 1;
+    private int numberOfDice;
    
-   public Dice(int numberOfDice) { // TODO: update class diagram
-      this.numberOfDice = numberOfDice;
-   }
-	public int[] roll(int numberOfDice){
-   int[] rollResult = new int[numberOfDice];
-		for (int i = 0; i < numberOfDice; i++) {
-         rollResult[i] = (int)(Math.random()*MAX_DICE_VALUE) + MIN_DICE_VALUE;
-      }
-      return rollResult;
-	}
+    public Dice(int numberOfDice) { // TODO: update class diagram
+       this.numberOfDice = numberOfDice;
+    }
+    public int[] roll(int numberOfDice){
+        int[] rollResult = new int[numberOfDice];
+        for (int i = 0; i < numberOfDice; i++) {
+            rollResult[i] = (int)(Math.random()*MAX_DICE_VALUE) + MIN_DICE_VALUE;
+        }
+        return rollResult;
+    }
 }
